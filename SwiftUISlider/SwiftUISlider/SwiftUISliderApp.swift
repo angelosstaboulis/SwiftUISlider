43f020c11dc223c018a77973a5baa9766cec92ae
Angelos Staboulis <angelosstaboulis@gmail.com>
2022-10-31 17:58:52 +0200

    Commit Initial Commit in SwiftUISlider

---
+++ SwiftUISlider/SwiftUISlider/SwiftUISliderApp.swift
@@ -0,0 +1,17 @@
+//
+//  SwiftUISliderApp.swift
+//  SwiftUISlider
+//
+//  Created by Angelos Staboulis on 31/10/22.
+//
+
+import SwiftUI
+
+@main
+struct SwiftUISliderApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
